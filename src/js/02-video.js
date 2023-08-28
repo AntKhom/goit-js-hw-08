a38905669b663a9f0dbc919512a7cc9f61693957
AntKhom <antonhomenko1988@gmail.com>
2023-08-28 12:29:48 -0400

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,13 @@ const iframe = document.querySelector('iframe');
 
 const player = new VimeoPlayer(iframe);
 
+
              
 player.on('timeupdate',
     throttle((data) =>
         localStorage.setItem("videoplayer-current-time", data.seconds), 1000) 
 );
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")===null ? 0 : localStorage.getItem("videoplayer-current-time"));
 
 
